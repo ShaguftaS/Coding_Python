b8cc126a6fbaa63da94bbf425bccc7ff3d26e43a
ShaguftaS <shaguftha.96@gmail.com>
2024-07-10 00:08:52 +0530

    Prog 1

--- SumOfPairs.py
+++ SumOfPairs.py
@@ -1,17 +1,18 @@
 def chkpair(a, size, x):
-    for i in range(0, size -1) :
-        for j in range(i+1, size) :
-            if (a[i] + a[j] == x) :
+    for i in range(0, size - 1):
+        for j in range(i + 1, size):
+            if (a[i] + a[j] == x):
                 return 1
 
     return 0
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     A = [0, -1, 2, -3, 1]
     x = -2
     size = len(A)
 
-    if (chkpair(A, size, x)) :
+    if chkpair(A, size, x):
         print("Yes")
 
     else:
